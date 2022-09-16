036a34352c0120c228042474c36d80e0b20fc6ae
Bruno Martins <brunoesm07@hotmail.com>
2022-09-16 18:45:35 -0300

    Resposta para o desafio 5.

---
+++ Desafio 5.py
@@ -0,0 +1,13 @@
+#Faca um programa que receba um número inteiro positivo N e calcule o somatório de 1 até N.
+#Imprima na tela o resultado da soma.
+
+n = int(input("Digite um número inteiro positivo: "))
+soma = int(n * (1 + n) / 2)
+
+print(soma)
+
+
+
+
+
+
