c3e49dba43005d22dffc317bcd904f8dc5baf7ed
Bruno Martins <brunoesm07@hotmail.com>
2022-09-17 18:36:31 -0300

    Inclusão de opção de resposta ao Desafio 5

--- Desafio 5.py
+++ Desafio 5.py
@@ -6,6 +6,15 @@ soma = int(n * (1 + n) / 2)
 
 print(soma)
 
+#Opção 2:
+
+n = int(input("Digite um número inteiro positivo: "))
+soma = 0
+for i in range(1, n+1):
+    soma += i
+
+print(soma)
+
 
 
 
---
+++ Desafio 6.py
@@ -0,0 +1,3 @@
+cateto1 = input("Digite o valor de um dos catetos: ")
+cateto2 = input("Digita o valor do outro cateto: ")
+hipotenusa =
