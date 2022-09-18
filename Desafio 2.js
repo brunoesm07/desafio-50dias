505694ebd1f377ef0a0755cf7481b0b65544cea3
brmartin | Bruno Martins <69209788+brunoesm07@users.noreply.github.com>
2022-09-18 18:03:24 -0300

    Update Desafio 2.js

--- Desafio 2.js
+++ Desafio 2.js
@@ -5,15 +5,13 @@ var inteiro3 = prompt("Insira o terceiro numero: ")
 var maior = inteiro1
 if (inteiro2 > maior)
     maior = inteiro2
-else if (inteiro3 > maior)
+if (inteiro3 > maior)
     maior = inteiro3
 
 var menor = inteiro1
 if (inteiro2 < menor)
 menor = inteiro2
-else if (inteiro3 < menor)
+if (inteiro3 < menor)
 menor = inteiro3
 
 alert("O maior numero e " + maior + " e o menor e " + menor)
-
-
