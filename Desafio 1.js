65009f05f1de6abaa81b4fc4beb0afdd0d9e4ee5
brmartin | Bruno Martins <69209788+brunoesm07@users.noreply.github.com>
2022-09-21 19:54:35 -0300

    Delete Desafio 1.js

--- Desafio 1.js
+++
@@ -1,6 +0,0 @@
-var preco = prompt("Insira o preco do produto: ")
-var pagamento = prompt("Insira o valor pago em dinheiro: ")
-
-var troco = pagamento - preco
-
-alert("Seu troco é de R$ " + troco)
