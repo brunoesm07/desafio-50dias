c0305d3976c27e9e20e58add85524849976b056f
brmartin | Bruno Martins <69209788+brunoesm07@users.noreply.github.com>
2022-09-21 19:57:21 -0300

    Delete Desafio9.py

--- Desafio9.py
+++
@@ -1,6 +0,0 @@
-dias_trab = int(input("Insira a quantidade de dias trabalhados: "))
-anos = int(dias_trab / 360)
-meses = int((dias_trab % 360) / 30)
-dias = int(dias_trab % 30)
-
-print("O funcionário trabalhou {} ano(s), {} mes(es) e {} dia(s)." .format(anos, meses, dias))
