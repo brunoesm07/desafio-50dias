8ebc358155618e99d8dd51011a34be9171796ba5
brmartin | Bruno Martins <69209788+brunoesm07@users.noreply.github.com>
2022-09-21 19:55:10 -0300

    Delete Desafio 1.py

--- Desafio 1.py
+++
@@ -1,6 +0,0 @@
-#Faça um programa que calcule a diferença entre o preço de um produto e o valor em dinheiro pago, imprimindo para o usuário o quanto será o troco
-preco = input("Preço do produto: ")
-pagamento = input("Valor pago em dinheiro: ")
-troco = float(pagamento) - float(preco)
-
-print(f"Seu troco é de {troco}")
